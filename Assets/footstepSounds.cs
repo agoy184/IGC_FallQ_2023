0d1ce74e426aeeefcd60f8bf5ad8769e9781d8f5
Luckypad101 <132636847+Luckypad101@users.noreply.github.com>
2023-11-14 15:12:07 -0800

    WalkingSounds
    customizable walking sound script where you can give objects certain tags and if the player walks over them it'll play the corresponding sounds

---
+++ Assets/footstepSounds.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footstepSounds : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip[] stepSounds;
+    public string[] soundTags;
+    public Transform groundCheck;
+    float castDistance = 1.5f;
+
+    bool moving;
+    // Update is called once per frame
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("moving");
+            moving = true;
+        }
+        else
+        {
+            moving = false;
+        }
+
+        if (!moving)
+        {
+            return;
+        }
+        RaycastHit hit;
+        if (Physics.Raycast(groundCheck.position, Vector3.down, out hit, castDistance))
+        {
+            Debug.Log(hit.collider.tag);
+            for (int i = 0; i < stepSounds.Length; i++)
+            {
+                
+                if(hit.collider.tag == soundTags[i])
+                {
+                    Debug.Log("working");
+                    audioSource.clip = stepSounds[i];
+                    if (!audioSource.isPlaying)
+                    {
+                        audioSource.Play();
+                    }
+                }
+            }
+
+        }
+        else
+        {
+            audioSource.Pause();
+            Debug.Log("not wokring");
+        }
+    }
+}
