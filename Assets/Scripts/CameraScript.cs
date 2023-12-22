4e9260789275fd561ed8f744cc2765b4f266b33b
Abel Goy <agoy@ucsc.edu>
2023-12-21 20:12:01 -0800

    end screen added, not in game yet

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -29,8 +29,8 @@ public class CameraScript : MonoBehaviour
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
-        yRotation += mouseX;
-        xRotation -= mouseY;
+        yRotation += (mouseX * 0.1f);
+        xRotation -= (mouseY * 0.1f);
 
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
